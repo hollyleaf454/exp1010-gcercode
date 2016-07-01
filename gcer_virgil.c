f007851f30f6b5c9bbfbd791d0118e1108ef146c
Elvin <whyu.tilt@gmail.com>
2016-07-01 15:33:21 -0400

    Update gcer_virgil.c
    made some changes

--- gcer_virgil.c
+++ gcer_virgil.c
@@ -19,16 +19,16 @@ void lineFollow()
 	{
 		if(analog(0) > 2000)
 		{
-			mav(lmotor, 800);
-			mav(rmotor, 1200);
-			msleep(100); // this can probably be faster
+			mav(lmotor, 730);
+			mav(rmotor, 600);
+			msleep(50);
 		}
 		
 		else
 		{
-			mav(lmotor, 1200);
-			mav(rmotor, 800);
-			msleep(100);
+          	mav(lmotor, 650);
+			mav(rmotor, 1000);
+			msleep(50); // this can probably be faster
 		}
 	}
 	printf("line follow donezo\n");
@@ -36,8 +36,8 @@ void lineFollow()
 
 int main()
 {
-    /*wait_for_light(1);
-    shut_down_in(115);*/
+    //wait_for_light(1);
+    //shut_down_in(115);
     enable_servos();
     //1) Grab Cubes
     /*set_servo_position(end, 300); //open grabbing part
@@ -50,9 +50,9 @@ int main()
   */set_servo_position(0, 2047);
   	set_servo_position(1, 1500);
     //2) Exit Starting Box
-  	mav(lmotor, 800);
-  	mav(rmotor, 800);
-  	msleep(4100);
+  	mav(lmotor, 1500);
+  	mav(rmotor, 1500);
+  	msleep(1950);
   
     printf("Get out\n");
     //3) Turn Towards Center of Field
@@ -68,8 +68,8 @@ int main()
     printf("...To the wall\n");
   	while(!(digital(button)))
     {
-        mav(lmotor, 800); //drive forwards until wall is hit
-        mav(rmotor, 800);
+        mav(lmotor, 1200); //drive forwards until wall is hit
+        mav(rmotor, 1200);
         msleep(700);
     }
     //5) Turn Towards Ramp
@@ -80,7 +80,7 @@ int main()
   
     mav(lmotor, -820);
 	mav(rmotor, 850); // turn in front of our ramp
-	msleep(910);
+	msleep(820);
     
   	set_servo_position(1, 900);
   	msleep(300);	
@@ -89,7 +89,7 @@ int main()
 	mav(rmotor, 1000); // drive up the ramp a little bit
   	msleep(2200);
   
-  	set_servo_position(1, 1600);
+  	set_servo_position(1, 1640);
     //6) Drive Up Ramp until Hits Edge
     printf("Up & up\n");
   	/*set_servo_position(front, 500); //raise front part a little bit (for some reason???)
@@ -110,17 +110,24 @@ int main()
 	mav(rmotor, 500);//turn left and check the solar array
   	while(analog(0) < 2000){msleep(10);}
   
-  	mav(lmotor, -550);
+  	mav(lmotor, -525);
   	mav(rmotor, -400);
-  	msleep(1500);
+  	msleep(1000);
 	
+  	set_servo_position(1, 1645);
+  	msleep(100);
+  
+  	mav(lmotor, -500);
+	mav(rmotor, 500);//turn left and check the solar array
+  	msleep(200);
+  
   	mav(lmotor, 300);//push things
-  	mav(rmotor, 300);
+  	mav(rmotor, 315);
   	msleep(13000);
   
   	mav(lmotor, -100);
   	mav(rmotor, -100);
-    msleep(100);
+    	msleep(100);
   
 	// spin arm to hit the thing
   	int i;
